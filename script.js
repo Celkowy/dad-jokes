a4acfbd448e92022bf95736970aed2b2a5af4013
tucelek@gmail.com <tucelek@gmail.com>
2020-12-28 18:09:14 +0100

    then replaced

--- script.js
+++ script.js
@@ -7,14 +7,16 @@ button.addEventListener('click', () => {
   getJoke()
 })
 
-function getJoke() {
+async function getJoke() {
   const config = {
     headers: {
       Accept: 'application/json',
     },
   }
 
-  fetch('https://icanhazdadjoke.com', config)
-    .then(res => res.json())
-    .then(data => (h1.innerHTML = data.joke))
+  const res = await fetch('https://icanhazdadjoke.com', config)
+  console.log(res)
+  const data = await res.json()
+
+  h1.innerHTML = data.joke
 }
